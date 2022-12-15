77a20712dc1b470dbe08522761d5d80417a0e6c4
Tori <76889884+korewatori@users.noreply.github.com>
2022-12-15 22:04:48 +0000

    Update get_image_id_urls.py

--- get_image_id_urls.py
+++ get_image_id_urls.py
@@ -9,9 +9,10 @@ ALBUM_URL_REGEX = re.compile(r"https://imx.to/g/[a-zA-Z0-9]{4,}")
 # Parse the command-line arguments using argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("album_url", help="URL of the imx.to album")
+parser.add_argument("-o", "--output", help="Path to the output file (optional)")
 args = parser.parse_args()
 
-# Check if the URL is a valid imx.to album URL or not
+# Check if the URL is a valid imx.to album URL
 if not ALBUM_URL_REGEX.match(args.album_url):
     print("Error: Invalid imx.to album URL")
     exit(1)
@@ -28,9 +29,20 @@ seen_urls = set()
 # Find all anchor tags
 links = soup.find_all("a")
 
-# Loop through the anchor tags and print the href attribute if it starts with "https://imx.to" and we haven't seen it before
-for link in links:
-    href = link["href"]
-    if href.startswith("https://imx.to") and href not in seen_urls:
-        print(href)
-        seen_urls.add(href)
+# If the output file was specified, open it for writing
+if args.output:
+    with open(args.output, "w") as f:
+        # Loop through the anchor tags and write the href attribute to the output file if it starts with "https://imx.to" and we haven't seen it before
+        for link in links:
+            href = link["href"]
+            if href.startswith("https://imx.to") and href not in seen_urls:
+                f.write(href + "\n")
+                seen_urls.add(href)
+# If the output file was not specified, print the URLs to the console
+else:
+    # Loop through the anchor tags and print the href attribute if it starts with "https://imx.to" and we haven't seen it before
+    for link in links:
+        href = link["href"]
+        if href.startswith("https://imx.to") and href not in seen_urls:
+            print(href)
+            seen_urls.add(href)
