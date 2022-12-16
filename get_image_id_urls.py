06b4fa41151d921425400e10555a6e7dd43bfe5c
Tori <76889884+korewatori@users.noreply.github.com>
2022-12-16 01:27:13 +0000

    Update get_image_id_urls.py
    Added the ability to get various info about the album URL provided, such as the total file size of all the files in the album, the title of the album and also the number of files.

--- get_image_id_urls.py
+++ get_image_id_urls.py
@@ -8,6 +8,7 @@ ALBUM_URL_REGEX = re.compile(r"https://imx.to/g/[a-zA-Z0-9]{4,}")
 
 # Parse the command-line arguments using argparse
 parser = argparse.ArgumentParser()
+parser.add_argument("command", help="Action to perform (info or links)")
 parser.add_argument("album_url", help="URL of the imx.to album")
 parser.add_argument("-o", "--output", help="Path to the output file (optional)")
 args = parser.parse_args()
@@ -22,23 +23,43 @@ response = requests.get(args.album_url)
 # Parse the HTML using BeautifulSoup
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Create a set to store the URLs that we have already seen
-seen_urls = set()
+if args.command == "info":
+    # Find the title of the album by searching for the 'h1' element with the class 'post-title'
+    title_element = soup.select('.title')[0]
+    title = title_element.text
 
-# Find all anchor tags
-links = soup.find_all("a")
+    # Find the image count by searching for the element with the '.slider-wrapper > div:nth-child(2) > div:nth-child(1) > span:nth-child(1)' CSS selector
+    image_count_element = soup.select('.slider-wrapper > div:nth-child(2) > div:nth-child(1) > span:nth-child(1)')[0]
+    image_count = image_count_element.text
 
-# If the output file was specified, open it for writing
-if args.output:
-    with open(args.output, "w") as f:
+    # Find the file size by searching for the element with the '.slider-wrapper > div:nth-child(2) > div:nth-child(1) > span:nth-child(2)' CSS selector
+    file_size_element = soup.select('.slider-wrapper > div:nth-child(2) > div:nth-child(1) > span:nth-child(2)')[0]
+    file_size = file_size_element.text
+
+    # Print the information we gathered
+    print(f'Title: {title}')
+    print(f'Number of files: {image_count}')
+    print(f'Size: {file_size}')
+    print(f'URL: {args.album_url}')
+
+elif args.command == "links":
+    # Create a set to store the URLs that we have already seen
+    seen_urls = set()
+
+    # Find all anchor tags
+    links = soup.find_all("a")
+
+    # If the output file was specified, open it for writing
+    if args.output:
+        with open(args.output, "w") as f:
+            for link in links:
+                href = link["href"]
+                if href.startswith("https://imx.to") and href not in seen_urls:
+                    f.write(href + "\n")
+                    seen_urls.add(href)
+    else:
         for link in links:
             href = link["href"]
             if href.startswith("https://imx.to") and href not in seen_urls:
-                f.write(href + "\n")
+                print(href)
                 seen_urls.add(href)
-else:
-    for link in links:
-        href = link["href"]
-        if href.startswith("https://imx.to") and href not in seen_urls:
-            print(href)
-            seen_urls.add(href)
